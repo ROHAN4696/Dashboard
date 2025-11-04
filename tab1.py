1201121ae43aebaf0ff3396eb171966cf7d4bf33
Priyanshu9382 <priyanshumishra419@gmail.com>
2025-11-04 15:26:48 +0530

    Added pie chart showing movies and Tv shows

--- tab1.py
+++ tab1.py
@@ -1,3 +1,21 @@
 import streamlit as st
+import plotly.express as px
 
 st.title("Netflix Dataset Viewer")
+
+df = st.session_state['netflix_df']
+type_counts = df['type'].value_counts().reset_index()
+type_counts.columns = ['Type', 'Count']
+
+fig = px.pie(
+    type_counts,
+    names='Type',
+    values='Count',
+    color='Type',
+    color_discrete_sequence=px.colors.qualitative.Set2,
+    hole=0.4, 
+    title='Distribution of Movies vs TV Shows'
+)
+
+fig.update_traces(textinfo='percent+label')
+st.plotly_chart(fig, use_container_width=True)
